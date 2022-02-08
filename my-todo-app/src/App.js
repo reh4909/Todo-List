f70fd49fc2f1fc73068947c75431f6d587f167c5
Shaik Rehmath <reh4909@gmail.com>
2022-02-08 16:24:47 +0530

    Todo-App

---
+++ my-todo-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoApp from "./Components/TodoApp";
+
+function App() {
+  return (
+  <div className="App">
+  <span className="title">Todo List</span> <br />
+      <TodoApp />
+</div>
+);
+}
+export default App;
